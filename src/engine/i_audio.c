83e05d95f7a15ba862b018ff5fd8c42a79722b43
Tom Kidd <tomkidd@gmail.com>
2023-09-03 17:52:58 -0500

    Upgraded to full fat FluidSynth
    Required for 3.6.5.5 looks like
    
    Mac still builds and runs, with sound

--- src/engine/i_audio.c
+++ src/engine/i_audio.c
@@ -43,11 +43,7 @@
 #include <SDL2/SDL.h>
 #endif
 
-//#if !defined _WIN32 || __APPLE__ || __arm__ || __aarch64__
-//#include <fluidsynth.h>
-//#else
-#include <fluidlite.h> //ATSB: Fluidlite on WIN32/macOS and some other devices so we can distribute binaries without all the stupid dependencies
-//#endif
+#include <fluidsynth.h>
 
 #include "doomtype.h"
 #include "doomdef.h"
